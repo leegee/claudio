2a678992be99fa082ed349ed0d5e88089c8c5c36
Lee Goddard <leegee@gmail.com>
2017-10-18 12:53:28 +0200

    'tidy'

--- htdocs/cgi-bin/lib/Izel.pm
+++ htdocs/cgi-bin/lib/Izel.pm
@@ -1,49 +1,44 @@
 use strict;
 use warnings;
 
-#
-# Not an ideal DB - not linked to Magento,
-# and no links due to geospatial being only in MyISAM
-# and lots of repition, but optimal for searching.
-#
-
 package Izel;
 
+use Data::Dumper;
 use LWP::UserAgent();
 use Encode;
 use Log::Log4perl ':easy';
 use Text::CSV_XS;
+use DBI;
 
 use JSON::Any;
-my $Jsoner = JSON::Any->new;
-my $UA;
+my $TOTAL_COUNTIES_IN_USA = 3007;
 
-# The CSV is the list of SKUs that are in stock
-sub skus_from_csv {
-	TRACE "Enter";
-	my $no_paths = shift || 0;
-	my $path = shift;
-	die 'No path?' if not $path;
+our $CONFIG = {
+	geosku_table_name => 'geosku',
+	db_path => 'sqlite.db',
+};
 
-	require Text::CSV_XS;
-	my @skus;
-	my $csv = Text::CSV_XS->new ({ binary => 1, auto_diag => 1 });
-	open my $fh, "<:encoding(utf8)", $path
-		or LOGDIE "$path - $!";
-	while (my $row = $csv->getline ($fh)) {
-		my $sku = $row->[0];
-		$sku =~ s/^\s+//;
-		$sku =~ s/\s+$//;
-		push @skus, $no_paths?
-			$sku : join'/',Izel->_path_bits( $sku );
+sub new {
+	my $inv  = shift;
+	my $args = ref($_[0])? shift : {@_};
+	my $self = {
+		%$args,
+		sth		=> {},
+		ua		=> LWP::UserAgent->new,
+		jsoner	=> JSON::Any->new,
+	};
+
+	$self->{ua}->timeout(30);
+	$self->{ua}->env_proxy;
+
+	if ($self->{auth_string} and not $self->{auth_token}) {
+		($self->{auth_token}) = $self->{auth_string} =~ /access_token=([^&;]+)/;
 	}
-	close $fh;
-	TRACE "Read ", scalar(@skus), " skus";
 
-	return @skus;
+	return bless $self, ref($inv) ? ref($inv) : $inv;
 }
 
-=head2 create_fusion_csv_multiple
+=head2 update
 
 Creates multiple CSVs and a JSON index of which SKU is in
 which CSV, because Fusion Tables only imports small CSVs at
@@ -76,60 +71,38 @@ Directory.
 
 =cut
 
-sub create_fusion_csv_multiple {
+sub update {
 	TRACE "Enter";
+	my $self = shift;
 	my $args = ref($_[0])? shift : {@_};
-    $args->{number_of_output_files} ||= 5;
 
+    $args->{number_of_output_files} ||= 5;
 	if ($args->{include_only_skus}){
 		$args->{include_only_skus} = { map { $_ => 1 } @{$args->{include_only_skus}} }
 	}
+	$args->{separator} ||= ',';
+
+	$self->get_dbh;
 
 	my $fh_index = -1;
-	my $rows_done = 0;
-	my $az = {};
+	my @csv_paths;
+	my @table_ids,
+	my $initials_count = {};
 
 	my $res = {
 		skus2tableIndex => {},
-		csvs => [],
-		fusiontables => [],
-		# rows_done => 0,
-		# az => {}
 	};
 
-    # my $skus = load_sku2latin_from_csv($args);
+	$self->load_geo_sku_from_csv(
+		path				=> $args->{county_distributions_path},
+		separator			=> $args->{separator},
+		include_only_skus	=> $args->{include_only_skus},
+	);
 
-	open my $IN, "<:encoding(utf8)", $args->{county_distributions_path}
-		or LOGDIE "$! - $args->{county_distributions_path}";
+	my $csv_output = Text::CSV_XS->new ({ binary => 1, auto_diag => 1 });
+    $csv_output->eol("\n");
 
-	my $csv_bar = Text::CSV_XS->new({
-		sep_char 	=> '|',
-		binary 		=> 1,
-		auto_diag 	=> 1
-	});
-
-	while (my $row = $csv_bar->getline($IN)) {
-		my $sku  = uc $row->[0];
-		my $fips = $row->[1];
-		my ($state_num, $county_num) = $fips =~ (/^(\d{1,2})(\d{3})$/);
-		my $geo_id2 = $fips + 0;
-		# TRACE $geo_id2.'...'.$fips, "\n";
-		if ($args->{include_only_skus} and exists $args->{include_only_skus}->{$sku}){
-			++ $rows_done;
-			my $initial = substr $sku,0,1;
-			push @{$az->{$initial}->{$sku}}, $geo_id2;
-		}
-	}
-	close $IN;
-
-	my $csv_comma = Text::CSV_XS->new ({ binary => 1, auto_diag => 1 });
-    $csv_comma->eol("\n");
-
-	my @keys = sort {
-		scalar keys %{$az->{$b}}
-			<=>
-		scalar keys %{$az->{$a}}
-	} keys %$az;
+	my @keys = @{ $self->get_initials };
 
 	# Distribute volumes of SKUs across multiple files.
 	# Order output by volume.
@@ -141,38 +114,24 @@ sub create_fusion_csv_multiple {
 	my (@OUT, $fh);
 
 	for my $i (0 .. $args->{number_of_output_files} -1){
-		my $pathCsv = $dir .'/'. $i . '.csv';
-		push @{$res->{csvs}}, $pathCsv;
-		my $name = 'table_' . $i;
+		my $name = 'skus_table_' . $i;
 
-		publish_table_to_google({
-			name => $name,
-			auth_string => $args->{auth_string},
-			table => {
-				name => $name,
-				isExportable => 'true',
-				description => 'Table ' . $i,
-				columns => [ 
-					{
-						name => "GEO_ID2",
-						type => "STRING",
-						kind => "fusiontables#column",
-						columnId => 1
-					},
-					{
-						name => "SKU",
-						type => "STRING",
-						kind => "fusiontables#column",
-						columnId => 2
-					},
-				]
-			}
+		my $create_res = $self->publish_table_to_google({
+			table_name => $name,
+			table_number => $i,
 		});
-		
+
+		push @{$res->{response}}, $create_res;
+
+		my $pathCsv = $dir .'/'. $i . '.csv';
 		open $fh, ">:encoding(utf8)", $pathCsv or die "$! - $pathCsv";
-		push @OUT, $fh;
 		print $fh "GEO_ID2,SKU\n";
+		push @OUT, $fh;
+		push @csv_paths, $pathCsv;
+		push @table_ids, $create_res->{table_id} if $create_res->{table_id};
 	}
+
+	my $az;
 
 	while (@keys){
 		$fh_index ++;
@@ -188,7 +147,7 @@ sub create_fusion_csv_multiple {
 			foreach my $geo_id2 (@{
 				$az->{$initial}->{$sku}
 			} ){
-				$csv_comma->print( $OUT[$fh_index], [
+				$csv_output->print( $OUT[$fh_index], [
 					$geo_id2, $sku
 				]);
 			}
@@ -200,130 +159,102 @@ sub create_fusion_csv_multiple {
 
 	close $_ foreach @OUT;
 
+	foreach my $table ( @{$res->{response}} ) {
+		$self->add_rows_to_google(
+			table_id => $table->{tableId}, 
+			csv_path => shift @csv_paths,
+		);
+	}
+
 	INFO "Create $dir/index.js";
 	open $fh, ">:encoding(utf8)", "$dir/index.js" or die "$! - $dir/index.js";
-	my $jsonRes = $Jsoner->encode( $res );
+	my $jsonRes = $self->{jsoner}->encode( $res );
 	print $fh $jsonRes;
 	close $fh;
 
-	# return $rows_done, $az, $skus2tableIndex;
 	return $jsonRes;
 }
 
+sub add_rows_to_google {
+	TRACE "Enter";
+	my $self = shift;
+	my $args = ref($_[0])? shift : {@_};
+
+    my $url = 'https://www.googleapis.com/fusiontables/v2/tables'
+		. '/' . $args->{table_id} . '/import'
+		. '?' . $self->{auth_string};
+
+	INFO "Posting $args->{csv_path} to $url";
+	return $self->_post_blob( $url, $args->{csv_path} );
+}
 
 sub publish_table_to_google {
     TRACE "Enter";
+	my $self = shift;
     my $args = ref($_[0])? shift : {@_};
-    my $body = $Jsoner->encode($args);
 
-	if (!$UA){
-		$UA = LWP::UserAgent->new;
-		$UA->timeout(30);
-		$UA->env_proxy;
-	}
+	my $table = {
+		name => $args->{table_name},
+		isExportable => 'true',
+		description => 'Table ' . $args->{table_number},
+		columns => [ 
+			{
+				name => "GEO_ID2",
+				type => "STRING",
+				kind => "fusiontables#column",
+				columnId => 1
+			},
+			{
+				name => "SKU",
+				type => "STRING",
+				kind => "fusiontables#column",
+				columnId => 2
+			},
+		]
+	};
 
     my $url = 'https://www.googleapis.com/fusiontables/v2/tables'
-		. '?uploadType=media'
-		. '&name=' . $args->{name}
-		. '&' . $args->{auth_string};
+		. '?' . $self->{auth_string};
 
-	INFO 'Posting to ', $url;
-
-	my $response = $UA->post(
-		'http://search.cpan.org/', 
-		Content => $Jsoner->encode($args->{table})
-	);
-
-	if ($response->is_success) {
-		INFO $response->decoded_content;  # or whatever
-	}
-	else {
-		LOGDIE $response->status_line;
-	}
+	INFO "Posting '$args->{name}' to $url";
+	my $res = $self->_post_blob( $url, $table );
+	$res->{tableId} = $res->{content}->{tableId};
+	return $res;
 }
 
-=head2 (METHOD) create_fusion_csv
+sub _post_blob {
+	my ($self, $url, $blob) = @_;
 
-As create_fusion_csv_multiple but creates one file.
+	$self->{ua}->default_header( authorization => $self->{auth_token} ) if $self->{auth_token};
+	$self->{ua}->default_header( 'content-type' => 'application/json' );
 
-Also accepts C<log>, which if present ought to be a filehandle (ie STDOUT).
+	my $response = $self->{ua}->post(
+		$url, 
+		Content => $blob =~ /\n/ ? $self->{jsoner}->encode($blob) : $blob
+	);
 
-=cut
+	my $res = {};
 
-sub create_fusion_csv {
-    TRACE "Enter";
-    my $args = ref($_[0])? shift : {@_};
+	if ($response->is_success) {
+		$res->{content} = $self->{jsoner}->decode( $response->decoded_content );
+	} else {
+		$res->{error} = $response->status_line;
+	}
 
-    $args->{log}->print("Opening county distribution file, $args->{county_distributions_path}") if $args->{log};
-
-    open my $IN, "<:encoding(utf8)", $args->{county_distributions_path}
-        or LOGDIE "$! - $args->{county_distributions_path}";
-
-    my $rows_done = 0;
-    my $skus = {};
-
-    my $csv_bar = Text::CSV_XS->new({
-        sep_char    => '|',
-        binary      => 1,
-        auto_diag   => 1
-    });
-
-    while (my $row = $csv_bar->getline($IN)) {
-        my $sku  = uc $row->[0];
-        my $fips = $row->[1];
-        my ($state_num, $county_num) = $fips =~ (/^(\d{1,2})(\d{3})$/);
-        my $geo_id2 = $fips + 0;
-        # DEBUG 'FIPS '.$fips. ' = GEO_ID2 '.$geo_id2;
-        ++ $rows_done;
-        push @{$skus->{$sku}}, $geo_id2;
-        $args->{log}->print("Read $sku $geo_id2") if $args->{log};
-    }
-    close $IN;
-
-    my $csv_comma = Text::CSV_XS->new ({ binary => 1, auto_diag => 1 });
-    $csv_comma->eol ("\n");
-
-    my ($dir, $ext) = $args->{output_path} =~ /^(.+?)(\.[^.]+)?$/;
-	$dir ||= '';
-    if ($dir && !-d $dir) {
-        mkdir $dir or die "$! - $dir";
-    }
-    
-    my @OUT;
-    my $path = $dir .'/sku2geoid2' . '.csv';
-    open my $fh, ">:encoding(utf8)", $path or die "$! - $path";
-    $args->{log}->print("Writing $path") if $args->{log};
-
-    my $skus_done = 0;
-    foreach my $sku (sort keys %$skus ){
-        ++ $skus_done;
-        foreach my $geo_id2 (@{$skus->{$sku}} ){
-            $csv_comma->print(
-                $fh,
-                [ $geo_id2, $sku ]
-            );
-        }
-        $args->{log}->print("Wrote $skus_done SKUs") if $args->{log} and $skus_done % 100 == 1;
-    }
-
-    close $fh;
-
-    $args->{log}->print("Wrote a total of $skus_done SKUs") if $args->{log};
-
-    return $rows_done, $skus_done, $path;
+	return $res;
 }
 
 
 sub load_sku2latin_from_csv {
     my $args = shift;
     my $skus = {};
-    my $csv_comma = Text::CSV_XS->new ({ binary => 1, auto_diag => 1 });
+    my $csv_output = Text::CSV_XS->new ({ binary => 1, auto_diag => 1 });
     INFO "Reading ",$args->{sku2latin_path};
     
 	open my $IN, "<:encoding(utf8)", $args->{sku2latin_path}
         or LOGDIE "$! - $args->{sku2latin_path}";
     
-	while (my $row = $csv_comma->getline($IN)) {
+	while (my $row = $csv_output->getline($IN)) {
         my $sku = uc $row->[0];
         $sku =~ s/^\s+//;
         $sku =~ s/\s+$//;
@@ -335,6 +266,101 @@ sub load_sku2latin_from_csv {
     return $skus;
 }
 
+# The CSV is the list of SKUs that are in stock
+sub skus_from_csv {
+	TRACE "Enter";
+	my $no_paths = shift || 0;
+	my $path = shift;
+	die 'No path?' if not $path;
 
+	require Text::CSV_XS;
+	my @skus;
+	my $csv = Text::CSV_XS->new ({ binary => 1, auto_diag => 1 });
+	open my $fh, "<:encoding(utf8)", $path
+		or LOGDIE "$path - $!";
+	while (my $row = $csv->getline ($fh)) {
+		my $sku = $row->[0];
+		$sku =~ s/^\s+//;
+		$sku =~ s/\s+$//;
+		push @skus, $no_paths?
+			$sku : join'/',Izel->_path_bits( $sku );
+	}
+	close $fh;
+
+	TRACE "Read ", scalar(@skus), " skus";
+
+	return @skus;
+}
+
+sub get_dbh {
+	my $self = shift;
+	$self->{dbh} = DBI->connect("dbi:SQLite:dbname=$CONFIG->{db_path}","","");
+
+	# Check for our table:
+	my $sth = $self->{dbh}->table_info();
+	my @tables = $self->{dbh}->selectall_array($sth);
+
+	# If the scheme does not exist, create it:
+	if (not grep { $_->[2] eq $CONFIG->{geosku_table_name}  } @tables ) {
+		$sth = $self->{dbh}->prepare("CREATE TABLE $CONFIG->{geosku_table_name} (
+			GEO_ID2	BLOB,
+			SKU		VARCHAR(10)
+		)");
+		$sth->execute or die;
+		$self->{created_db} = 1;
+	} 
+	
+	else {
+		$self->{created_db} = 0;
+	}
+
+}
+
+sub insert_geosku {
+	my ($self, $geo_id2, $sku) = @_;
+
+	$self->{sth}->{insert_geosku} ||= $self->{dbh}->prepare(
+		"INSERT INTO $CONFIG->{geosku_table_name} (GEO_ID2, SKU) VALUES (?, ?)"
+	);
+
+	return $self->{sth}->{insert_geosku}->execute( $geo_id2, $sku );
+}
+
+
+sub load_geo_sku_from_csv {
+	my $self = shift;
+	my $args = ref($_[0])? shift : {@_};
+	$args->{separator} ||= ',';
+	my $count = 0;
+
+	open my $IN, "<:encoding(utf8)", $args->{path}
+		or LOGDIE "$! - $args->{path}";
+
+	my $csv_input = Text::CSV_XS->new({
+		sep_char 	=> $args->{separator},
+		binary 		=> 1,
+		auto_diag 	=> 1
+	});
+
+	while (my $row = $csv_input->getline($IN)) {
+		my $sku  = uc $row->[0];
+		my $fips = $row->[1];
+		die 'FIPS should be numeric' if not $fips =~ /^\d+$/;
+		my $geo_id2 = $fips + 0;
+		if (not($args->{include_only_skus}) or exists $args->{include_only_skus}->{$sku}){
+			$self->insert_geosku( $geo_id2, $sku );
+			$count ++;
+		}
+	}
+	
+	close $IN;
+	return $count;
+}
+
+sub get_initials {
+	return [
+		map {$_->[0]} shift->{dbh}->selectall_array("SELECT DISTINCT SUBSTR(sku,1,1) FROM  $CONFIG->{geosku_table_name} ORDER BY sku ASC")
+	];
+}
 
 1;
