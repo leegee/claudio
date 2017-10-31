65b01ae4c7fda06ee51a297f75bfe0c04bd3159a
Lee Goddard <leegee@gmail.com>
2017-10-31 11:35:16 +0100

    Separate config

---
+++ htdocs/Config.js
@@ -0,0 +1,15 @@
+{
+    "apikey": "AIzaSyDAdOILIN2SPM_1iPKJMbHd9jOck8H9J7o",
+    "clientId": "837054515213-gja7o8fkcc63vge1noitlu39ijjgrmtu.apps.googleusercontent.com",
+    "scopes": "https://www.googleapis.com/auth/fusiontables",
+    "fusion": {
+        "name": "lee-dev",
+        "countiesTableId": "1CP_uYV52MKV42Qt7O3TrpzS1sr7JBWPMIWxw4sQV",
+        "sheetNameStem": "lee-dev-",
+        "maxRetries": 3
+    },
+    "endpoints": {
+        "uploadSkus": "/cgi-bin/upload-skus.cgi"
+    },
+    "indexBaseDir": "temp"
+}
