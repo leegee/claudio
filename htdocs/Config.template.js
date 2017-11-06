a780bc4efa45a57788f2c52530548b07ce8d1471
Lee Goddard <leegee@gmail.com>
2017-11-06 11:01:03 +0100

    config

--- htdocs/Config.template.js
+++ htdocs/Config.template.js
@@ -1,6 +1,6 @@
 {
-    "apiKey": "AIzaSyDAdOILIN2SPM_1iPKJMbHd9jOck8H9J7o",
-    "clientId": "837054515213-almgarbjlt0tv1qc1fjem1hs4ui3mebr.apps.googleusercontent.com",
+    "apiKey": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    "clientId": "XXXXXXXXXXXXXXXXXXXXXXXXXXX.apps.googleusercontent.com",
     "discoveryUrls": ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"],
     "scopes": [
         "https://www.googleapis.com/auth/fusiontables",
@@ -9,7 +9,7 @@
     ],
     "fusion": {
         "name": "lee-dev",
-        "countiesTableId": "1CP_uYV52MKV42Qt7O3TrpzS1sr7JBWPMIWxw4sQV",
+        "countiesTableId": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
         "sheetNameStem": "lee-dev-",
         "maxRetries": 3
     },
