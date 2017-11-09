a55e57f1cb3137bbf5f5c23bcdf4b761db571f94
Lee Goddard <leegee@gmail.com>
2017-11-09 14:34:54 +0100

    remove stack trace

--- conf.js
+++ conf.js
@@ -1,5 +1,21 @@
+let SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+
 exports.config = {
+  jasmineNodeOpts: {
+    print: function () {}
+  },
   framework: 'jasmine',
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['test/one.js']
+  specs: ['tests/one.spec.js'],
+
+  onPrepare: function () {
+    jasmine.getEnv().addReporter(
+      new SpecReporter({
+        spec: {
+          displayStacktrace: false
+        }
+      })
+    );
+  }
 }
+
